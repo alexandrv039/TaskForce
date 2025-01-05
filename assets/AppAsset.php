e99861233b0b3e62c31bc8a698a7a47dce42a1fb
vinokurov <239@b2b-logist.com>
2025-01-05 17:39:47 +0500

    Вывод стартовой страницы

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -20,12 +20,14 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'css/normalize.css',
+        'css/style.css',
     ];
     public $js = [
+        'js/main.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap5\BootstrapAsset'
+//        'yii\bootstrap5\BootstrapAsset'
     ];
 }
